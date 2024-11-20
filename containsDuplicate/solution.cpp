75d88e25173cee69d1bc5e6ad0c33a8adcdde7c7
Vladislav Cernega <vladislav@walkcat.net>
2024-11-20 09:17:56 +0000

    217. Contains Duplicate (improved)

--- containsDuplicate/solution.cpp
+++ containsDuplicate/solution.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-    for (int i = 0; i < nums.size() - 1; i++)
-        for (int j = i + 1; j < nums.size(); j++)
-            if (nums[i] == nums[j]) return true;
-                else continue;
-    return false;
+    
+        unordered_set<int> numFreq;
+
+        for (int num : nums) {
+            if (numFreq.count(num)) return true;
+            else numFreq.insert(num);
+        }
+
+        return false;
     }
 };
