05f7fd0f3288d8547544889a9f076f039d9b4652
Vladislav Cernega <vladislav@walkcat.net>
2024-11-26 16:08:35 +0000

    2924. Find Champion II

--- findChampion/solution.cpp
+++ findChampion/solution.cpp
@@ -1,35 +1,55 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-       class Solution {
-public:
-    int findChampion(int n, vector<vector<int>>& edges) {
         vector<int> inDegree(n, 0);
         unordered_map<int, unordered_set<int>> graph;
 
+        // Build the graph and calculate in-degrees
         for (const auto& edge : edges) {
             int u = edge[0], v = edge[1];
             graph[u].insert(v);
             inDegree[v]++;
         }
 
+        // Find the node with no incoming edges
         int champion = -1;
         for (int i = 0; i < n; ++i) {
             if (inDegree[i] == 0) {
                 if (champion != -1) {
-                    return -1; // More than one team with no incoming edges
+                    return -1; // More than one node with no incoming edges
                 }
                 champion = i;
             }
         }
 
-        for (int i = 0; i < n; ++i) {
-            if (i != champion && graph[champion].find(i) == graph[champion].end()) {
-                return -1; // Not all nodes are reachable from the champion
+        // If no node with zero in-degree is found, return -1
+        if (champion == -1) {
+            return -1;
+        }
+
+        // Check if the "champion" can reach all other nodes
+        unordered_set<int> visited;
+        queue<int> q;
+        q.push(champion);
+
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
+            if (visited.count(current)) continue;
+
+            visited.insert(current);
+            for (const auto& neighbor : graph[current]) {
+                if (!visited.count(neighbor)) {
+                    q.push(neighbor);
+                }
             }
+        }
+
+        // If the number of visited nodes is not equal to n, return -1
+        if (visited.size() != n) {
+            return -1;
         }
 
         return champion;
     }
-}; 
-
+};
